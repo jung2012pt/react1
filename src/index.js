df2b811d9fd6e6dad091ae2552b045d6d17b49e1
CSTU-CS369-2022-2 <poompichayout.kon@gmail.com>
2023-02-23 08:24:44 +0700

    week7: counter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './AppFilter'
+import App from './AppCounter'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
